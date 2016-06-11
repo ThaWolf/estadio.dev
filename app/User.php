783ffaafa5792142f62331935e5f4b6a79413fec
Wolf <augusto.woelfert@gmail.com>
2016-06-11 13:40:49 -0300

    inscripcion funcionando

--- app/User.php
+++ app/User.php
@@ -26,7 +26,7 @@ class User extends Authenticatable
 
 public function torneos(){
 
-return $this->belongsToMany('App\Torneo');
+return $this->belongsToMany('App\Torneo' , 'torneos_users');
 
 }   
 
